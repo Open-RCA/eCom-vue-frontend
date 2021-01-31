c8e6326255986eb6218a7a084715bd2759dff9cb
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-31 10:48:19 +0200

    sample data to auth components

---
+++ src/components/Signin.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form-container sign-in-container">
+	<form action="#">
+		<h1>Sign In</h1>
+		<div class="social-container">
+		<a href="#" class="social"><i class="fa fa-facebook"></i></a>
+		<a href="#" class="social"><i class="fa fa-google"></i></a>
+		<a href="#" class="social"><i class="fa fa-linkedin"></i></a>
+	</div>
+	<span>or use your account</span>
+	<input type="email" name="email" placeholder="Email">
+	<input type="password" name="password" placeholder="Password">
+	<a href="#">Forgot Your Password</a>
+
+	<button>Sign In</button>
+	</form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Signin",
+//   props: {
+//     msg: String
+//   }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Auth.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Signin/>
+    <Signup/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Signin from "@/components/Signin.vue";
+import Signup from "@/components/Signup.vue";
+
+export default {
+  name: "Auth",
+  components: {
+    Signin,
+    Signup
+  }
+};
+</script>
