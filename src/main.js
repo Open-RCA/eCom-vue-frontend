c3efaf10e3d7646108890ec06950b9c68ce71759
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-31 17:19:03 +0200

    add vuesax configurations

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './assets/global.css';
-
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  Vuesax,
   store,
   render: h => h(App)
 }).$mount("#app");
