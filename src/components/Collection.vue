ede0b21978b91a5faf71de0a6f6f4225b0dd8fed
phinah <phinah.mahoro10@gmail.com>
2021-03-09 20:37:05 +0200

    started doing collections

---
+++ src/components/Collection.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="allcollected">
+        <h3 class="head">Our collections</h3>
+        <div id="collect">
+            <div class="text">
+                <h3 class="jordans">Jordans</h3>
+                <p>Jordans are the best shoes to best seen on this earth. 
+                    Try buying them and see how you fell happy.</p>
+                    <button class="onclick">Visit Collections</button>
+                    <div class="pagination">
+                        <span>
+                        <a href="#">1</a>
+                        <a href="#">2</a>
+                        <a href="#">3</a>
+                        <a href="#">4</a>
+                        <a href="#">5</a>
+                        </span>
+                    </div>
+            </div>
+            <div class="img1"><img src="../assets/images/shoe5@2x.png" alt="" width="222px" height="378px"></div>
+            <div class="img2"><img src="../assets/images/jordan-4657349@2x.png" alt="" width="468px" height="318px"></div>
+            <div class="img3"><img src="../assets/images/nike-5418992@2x.png" alt="" width="468px" height="312px"></div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Signin from "../views/Signin.vue";
 import Signup from "../views/Signup.vue";
+import collection from "../components/Collection.vue";
 
 Vue.use(VueRouter);
 
@@ -30,6 +31,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path:"/collect",
+    name:"Collections",
+    component: collection
   }
 ];
 
