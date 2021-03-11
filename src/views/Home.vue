9481c9320d5a024ee555d8cd7b2726cd8aa3380a
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-11 21:09:46 +0200

    added slider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <NavBar></NavBar>
+    <LandingSlider></LandingSlider>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/Navbar.vue";
+import LandingSlider from "@/components/LandingSlider.vue"
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    LandingSlider
   }
 };
 </script>
