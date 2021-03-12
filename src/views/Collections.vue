54eba0e64f39f63402e64026e3f2b3405adcdf67
yassin <hyassin509@gmail.com>
2021-03-12 08:58:11 +0200

    Merge branch 'Erneste-vue' of https://github.com/Open-RCA/eCom-vue-frontend into Yassin-Vue

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -5,23 +5,33 @@
       <h3 class="head">Jordan's collections</h3>
       </div>
 
-   <div v-for="product in Jordans" :key="product" class="images">
- <div class="jordan" v-for="jordan1 in jordan" :key="jordan1">
- <img v-bind:src="jordan1.imgUrl" alt="">
+      
+   <div v-for="Jordan in Jordan" :key="Jordan" class="images">
+  <div class="Jordan-image">
+  <img v-bind:src="Jordan.imgUrl" alt="Jordan-product-images">
+  </div>
+  <div class="Jordan-description">
+   <h3 class="name">{{Jordan.Name}}</h3>
+   <h3 class="price">{{Jordan.Price}}</h3>
+   <button>Add to cart  <img src="../assets/svg/Icon feather-plus-circle.svg" style="margin-left:0.7em;font-size:2x"></button>
  </div>
+  </div>
 
+<!-- Jordan collections -->
+
+   <div v-for="product in Jordans" :key="product" class="images">
   <div class="product-image">
   <img v-bind:src="product.imgUrl" alt="Jordan-product-images">
   </div>
-
   <div class="description">
    <h3 class="name">{{product.Name}}</h3>
    <h3 class="price">{{product.Price}}</h3>
-   <button>Add to cart  <img src="../assets/svg/plus.svg" style="margin-left:0.7em;font-size:2x"></button>
+   <button>Add to cart  <img src="../assets/svg/Icon feather-plus-circle.svg" style="margin-left:0.7em;font-size:2x"></button>
  </div>
   </div>
 
 
+<!-- men collections -->
  <h3 class="men-head">Men Collection</h3>
 
  <div class="man-collection" v-for="man in man" :key="man">
@@ -29,7 +39,6 @@
  </div>
 
 
-<!-- men collections -->
  <div v-for="man in mancollection" :key="man" class="man-images">
   <div class="man-product-image">
   <img v-bind:src="man.imgUrl" alt="Jordan-product-images">
@@ -38,19 +47,19 @@
    <h3 class="man-name">{{man.Name}}</h3>
    <h3 class="man-price">{{man.Price}}</h3>
 
-   <button>Add to cart  <img src="../assets/svg/plus.svg" style="margin-left:0.7em;font-size:2x"></button>
+   <button>Add to cart  <img src="../assets/svg/Icon feather-plus-circle.svg" style="margin-left:0.7em;font-size:2x"></button>
  </div>
   </div>
 
 
-  
+  <!-- Woman collections -->
  <h3 class="woman-head">Women Collection</h3>
  <div class="man-collection" v-for="woman in woman" :key="woman">
  <img v-bind:src="woman.imgUrl" alt="man-collection-image">
  </div>
 
 
-<!-- Woman collections -->
+
  <div v-for="woman in womancollection" :key="woman" class="man-images">
   <div class="man-product-image">
   <img v-bind:src="woman.imgUrl" alt="Jordan-product-images">
@@ -59,7 +68,7 @@
    <h3 class="man-name">{{woman.Name}}</h3>
    <h3 class="man-price">{{woman.Price}}</h3>
 
-   <button>Add to cart  <img src="../assets/svg/plus.svg" style="margin-left:0.7em;font-size:2x"></button>
+   <button>Add to cart   <img src="../assets/svg/Icon feather-plus-circle.svg" style="margin-left:0.7em;font-size:2x"></button>
  </div>
   </div>
 
@@ -79,13 +88,15 @@ export default {
 data(){
 
   return{
-    Jordans:[
+    Jordan:[
        {
             Name: "Jordan Air IV",
             Price: "$ 1000",
             Rating: 7,
             imgUrl:require("../assets/images/j1.png")
        },
+    ],
+    Jordans:[
 
         {
             Name: "Jordan Purple blue",
