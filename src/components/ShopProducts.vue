3c95b831d5428f05256f7c2885e610a4c0331d55
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-15 10:52:14 +0200

    required components added

--- src/components/ShopProducts.vue
+++ src/components/ShopProducts.vue
@@ -1,15 +1,27 @@
 <template>
+  <div>
     <div>
-      <h1>Shop Products</h1>
+      <ShopListingFilter />
     </div>
+    <div>
+      <ShopListingSearch />
+      <ShopListing />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  components: {
+    ShopListing: () => import("@/components/ShopProductsListing"),
+    ShopListingFilter: () => import("@/components/ShopListingFilter"),
+    ShopListingSearch: () => import("@/components/ShopListingSearch"),
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+div {
+  background-color: blue;
+}
 </style>
---
+++ src/components/ShopProductsListing.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>from Shop listing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: "Product 1",
+          price: "200",
+          imageUrl: "@/assets/images/j1.png",
+        },
+        {
+          id: 2,
+          title: "Product 1",
+          price: "200",
+          imageUrl: "@/assets/images/j2.png",
+        },
+        {
+          id: 3,
+          title: "Product 1",
+          price: "200",
+          imageUrl: "@/assets/images/j3.png",
+        },
+        {
+          id: 4,
+          title: "Product 1",
+          price: "200",
+          imageUrl: "@/assets/images/j4.png",
+        },
+        {
+          id: 5,
+          title: "Product 1",
+          price: "200",
+          imageUrl: "@/assets/images/j5.png",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
