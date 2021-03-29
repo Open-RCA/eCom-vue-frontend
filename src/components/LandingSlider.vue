9eb8b1e3380e7fcd0c701e84aa9cac2744771931
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-29 18:25:35 +0200

    finished landing slider design

--- src/components/LandingSlider.vue
+++ src/components/LandingSlider.vue
@@ -5,16 +5,16 @@
         <div class="slider__shop_text">
           <h1>O shop</h1>
           <p>
-            We sell, anything to you and in great quality.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorem.
+            We sell, anything to you and in great quality. Lorem, ipsum dolor
+            sit amet consectetur adipisicing elit. Itaque dolorem.
           </p>
         </div>
         <div class="slider__content">
           <div>
             <h4>The Nike Jordan 1</h4>
             <p>
-              Lorem ipsum dolor, sit amet consec tetur adipisicing elit
-              ipsum dolor, cons.
+              Lorem ipsum dolor, sit amet consec tetur adipisicing elit ipsum
+              dolor, cons.
             </p>
             <button type="button">Buy Now</button>
           </div>
@@ -22,41 +22,67 @@
       </div>
       <div class="slider__right col-12 col-md-7 col-lg-8">
         <div class="controls__container">
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M8 12l6-6v12z"/></svg>
+          <button class="btn-control">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M8 12l6-6v12z" />
+            </svg>
           </button>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M16 12l-6 6V6z"/></svg>
+          <button class="btn-control">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M16 12l-6 6V6z" />
+            </svg>
           </button>
         </div>
         <div class="image_container">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="662.078" height="585.204" viewBox="0 0 1162.078 1085.204">
-              <path id="Polygon_1" data-name="Polygon 1" d="M382.858,156.652c40-69.507,140.287-69.507,180.283,0L856.308,666.13C896.2,735.463,846.159,822,766.167,822H179.833C99.841,822,49.8,735.463,89.692,666.13Z" transform="translate(0 307.987) rotate(-19)"/>
+              <path
+                id="Polygon_1"
+                data-name="Polygon 1"
+                d="M382.858,156.652c40-69.507,140.287-69.507,180.283,0L856.308,666.13C896.2,735.463,846.159,822,766.167,822H179.833C99.841,822,49.8,735.463,89.692,666.13Z"
+                transform="translate(0 307.987) rotate(-19)"
+              />
             </svg>
+            <img src="../assets/images/shoe4.png" alt="shoe">
           </div>
-          <div>
-            dot controls
+          <div class="dot-controls">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
       </div>
       <div class="absolute__shop_text">
-        hjk
+        <h1>O shop</h1>
+        <p>
+          We sell anything to you and in great quality.
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod
+        </p>
       </div>
     </div>
-    <br>
+    <br />
   </div>
 </template>
 
 <script>
 export default {
- name: "LandingSlider",
- data(){
-   return {
-     intro_text: "We sell anything to you in great quality"
-   }
- }
-}
+  name: "LandingSlider",
+  data() {
+    return {
+      intro_text: "We sell anything to you in great quality",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -75,16 +101,18 @@ export default {
     display: flex;
     flex-direction: row;
   }
-  .slider__left, .slider__right {
+  .slider__left,
+  .slider__right {
     min-height: 90vh;
   }
   .slider__left {
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
+    justify-content: center;
     padding-bottom: 4%;
 
-    .slider__content,.slider__shop_text {
+    .slider__content,
+    .slider__shop_text {
       display: flex;
       justify-content: center;
       align-items: center;
@@ -96,29 +124,33 @@ export default {
       text-align: left;
       padding-bottom: 1%;
 
-      h1,p {
+      h1,
+      p {
         width: 100%;
         text-align: left;
+      }
+      h1 {
+        margin-bottom: 5%;
       }
       p {
         font-size: 99%;
         line-height: 130%;
         padding: 0;
-        color: rgb(46, 46, 46)
+        color: rgb(46, 46, 46);
       }
     }
     .slider__content {
       div {
         width: 100%;
         background-color: #000;
-        padding: 2% 5%;
-        color: #FEFEFE;
-        padding-bottom: 8%;
-        
+        padding: 3% 5%;
+        color: #fefefe;
+        padding: 8% 6%;
+
         p {
-          margin: 0;
+          margin: 10px 0;
           padding: 0;
-          font-size: 95%
+          font-size: 95%;
         }
         button {
           flex: none;
@@ -128,6 +160,8 @@ export default {
           font-weight: bold;
           width: 100%;
           margin-left: 0;
+          height: 42px;
+          margin-top: 20px;
         }
       }
     }
@@ -138,7 +172,8 @@ export default {
     flex-direction: row;
     padding: 0 0;
 
-    .image_container,.controls__container {
+    .image_container,
+    .controls__container {
       display: flex;
     }
     .controls__container {
@@ -157,13 +192,7 @@ export default {
         border-radius: 50%;
         cursor: pointer;
         margin-left: 0;
-
-        &:hover, &:hover svg {
-          background: black;
-          border: 1px solid black;
-          color: white;
-          fill: white;
-        }
+        margin-top: 20px;
       }
     }
     .image_container {
@@ -172,7 +201,6 @@ export default {
       position: relative;
       flex-direction: column;
       padding: 0 0;
-      border: 1px solid rgb(1, 0, 49);
 
       div {
         width: 100%;
@@ -182,11 +210,51 @@ export default {
         justify-content: flex-end;
         align-items: flex-end;
 
+        img {
+          width: 100%;
+          position: absolute;
+          transform: rotate(10deg);
+        }
+
         svg {
-          margin-top: -50%;
+          margin-top: -33%;
           padding: 0 0;
-          width: 120%;
-          height: 120%;
+          width: 110%;
+          height: 110%;
+        }
+      }
+
+      .dot-controls {
+        display: flex;
+        justify-content: flex-end;
+        flex-direction: row;
+        padding-right: 4%;
+        column-gap: 2%;
+
+        div {
+          width: 25px;
+          height: 25px;
+          background-color: #000;
+          border-radius: 50%;
+          text-align: center;
+          display: flex;
+          justify-content: center;
+          padding: 0;
+          cursor: pointer;
+          background-color: #C8C8C8;
+          transition-duration: 0.5s;
+
+          &:nth-child(1) {
+            background-color: #000;
+            width: 30px;
+            height: 30px;
+          }
+
+          &:hover {
+            background-color: #000;
+            width: 30px;
+            height: 30px;
+          }
         }
       }
     }
@@ -194,11 +262,33 @@ export default {
   .absolute__shop_text {
     display: none;
     position: absolute;
-    top: 0;
-    left: 0;
-    background: rgb(255, 207, 207);
+    top: -70px;
+    left: -60px;
+    background: #646464;
+    color: white;
+    line-height: 130%;
+    max-width: 300px;
+    height: 300px;
+    padding: 2%;
+    padding-top: 22%;
+    padding-left: 16%;
+    border-radius: 50%;
+    font-size: 100%;
+
+    p {
+      font-size: 90%;
+    }
   }
 }
+
+// .btn-control:hover,
+// .btn-control:hover svg {
+//   background: black;
+//   border: 1px solid black;
+//   border-radius: 50%;
+//   color: white;
+//   fill: white;
+// }
 
 /** For Small screens */
 @media screen and (max-width: 768px) {
@@ -207,7 +297,8 @@ export default {
     .slider-container {
       flex-direction: column-reverse;
     }
-    .slider__left, .slider__right {
+    .slider__left,
+    .slider__right {
       min-height: auto;
       height: auto;
     }
@@ -220,7 +311,7 @@ export default {
       // }
     }
     .slider__right {
-      padding-top: 30vh;
+      padding-top: 250px;
       flex-direction: row-reverse;
 
       .controls__container {
