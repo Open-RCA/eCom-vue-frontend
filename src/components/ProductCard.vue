9b2107ef94ce61732dd3f02a5d2af447dff4b182
MikeManzi <manzimike378@gmail.com>
2021-04-26 21:22:30 +0200

    added products' swiper

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,27 +1,48 @@
 <template>
   <div class="card-container row">
-      <div class="product-card">
-          <div class="product-image">
-              <img src="../../public/bottle2@3x.png" alt="">
-          </div>
-          <div class="product-details">
-              <p class="product-name">Quokka Bottle</p>
-              <div class="rating"><rating-stars v-bind="config"></rating-stars></div>
-              <p>Bottle</p>
-              <p>$500</p>
-              <button class="cart-btn">Add to cart<img src="../assets/svg/plus.svg"></button>
-          </div>
-      </div>
+      <swiper class="swiper" :options="swiperOption">
+        <swiper-slide v-for="(product,i) in products" :key="i">
+            <div class="product-card"  >
+                <div class="product-image">
+                    <img src="../../public/bottle2@3x.png" alt="">
+                </div>
+                <div class="product-details">
+                    <p class="product-name">{{product.name}}</p>
+                    <div class="rating"><rating-stars v-bind="config"></rating-stars></div>
+                    <p>{{product.description}}</p>
+                    <p>${{product.price}}</p>
+                    <button class="cart-btn">Add to cart
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17">
+  <g id="Icon_feather-plus-circle" data-name="Icon feather-plus-circle" transform="translate(-2.5 -2.5)">
+    <path id="Path_15" data-name="Path 15" d="M19,11a8,8,0,1,1-8-8A8,8,0,0,1,19,11Z" fill="none" stroke="#fafafa" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+    <path id="Path_16" data-name="Path 16" d="M18,12v6.4" transform="translate(-7 -4.2)" fill="none" stroke="#fafafa" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+    <path id="Path_17" data-name="Path 17" d="M12,18h6.4" transform="translate(-4.2 -7)" fill="none" stroke="#fafafa" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"/>
+
+  </g>
+</svg>
+                    </button>
+                </div>
+            </div>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
   </div>
 </template>
 
 <script>
 import ratingStars from "./RatingStars"
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 export default {
     name: "Product",
     components: {
-    ratingStars: ratingStars
+    ratingStars,  
+    Swiper,
+    SwiperSlide,
     },
+    props: ['products'],
     data(){
         return{
             config: {
@@ -33,6 +54,21 @@ export default {
                     starWidth: 15,
                     starHeight: 15
                 }
+            },
+            swiperOption: {
+                slidesPerView: 5,
+                spaceBetween: 2,
+                slidesPerGroup: 1,
+                loop: true,
+                loopFillGroupWithBlank: false,
+                pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+                },
+                navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+                }
             }
         }
     }
@@ -41,18 +77,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "./base.scss";
 .card-container{
     font-family: Raleway;
     background: #F1F1F1;
-    widows: 100%;
+    //widows: 100%;
     height: 100vh;
     margin: 0;
     padding: 0;
+    .swiper{
+        box-shadow: none;
+        background-color: transparent;
+        width: 92vw;
+    }
     .product-card{
         
         // max-width: 18%;
         height: 270px;
         box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.16);
+        margin: 15px 17px;
         &:hover{
             cursor: pointer;
         }
@@ -61,7 +104,7 @@ export default {
             img{
                 height: 230px;
                 width: 270px;
-                margin: -20px -18px 0;
+                margin: -20px -30px 0;
             }
         }
         .product-details{
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,68 @@
 <template>
   <div class="home">
-    <NavBar></NavBar>
     <LandingSlider></LandingSlider>
+    <h2>New Arrivals</h2>
+    <ProductCard v-bind:products="products"></ProductCard>
+    <h2>Our Products</h2>
+    <ProductCard v-bind:products="newArrivals"></ProductCard>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/Navbar.vue";
 import LandingSlider from "@/components/LandingSlider.vue"
+import ProductCard from "@/components/ProductCard.vue"
+
 
 export default {
+  
   name: "Home",
   components: {
-    NavBar,
-    LandingSlider
+    // NavBar,
+    LandingSlider,
+    ProductCard,
+  },
+  data(){
+    return{
+      newArrivals:[
+        {name: "Bottle", description: "bottle", price: 500},
+        {name: "Bottle", description: "bottle", price: 500},
+        {name: "Bottle", description: "bottle", price: 500},
+        {name: "Bottle", description: "bottle", price: 500},
+        {name: "Bottle", description: "bottle", price: 500},
+        {name: "Bottle", description: "bottle", price: 500},
+      ],
+      products:[
+        {name: "Quokka Bottle", description: "bottle", price: 500},
+        {name: "Quokka Bottle", description: "bottle", price: 500},
+        {name: "Quokka Bottle", description: "bottle", price: 500},
+        {name: "Quokka Bottle", description: "bottle", price: 500},
+        {name: "Quokka Bottle", description: "bottle", price: 500},
+        {name: "Quokka Bottle", description: "bottle", price: 500},
+      ],
+      swiperOption: {
+        slidesPerView: 5,
+        spaceBetween: 2,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: false,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      }
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
-// .home {
-//   background: rgb(147, 212, 255);
-// }
+
+.home {
+  h2 {
+      margin-left: 65px;
+  }
+}
 </style>
